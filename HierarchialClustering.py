da194feb0ea6767e247bdbb8164957652215871f
Ashwin Nikam <anikam@buffalo.edu>
2017-10-21 01:40:09 -0400

    added some functionalities

--- HierarchialClustering.py
+++ HierarchialClustering.py
@@ -1,4 +1,5 @@
 import numpy as np
+import sys
 
 file = open("../../Desktop/iyer.txt")
 lines = file.readlines()
@@ -16,7 +17,6 @@ matrix = np.delete(matrix, 0, 1)
 #Created a matrix of the data set
 #Created an array of true values
 
-
 def calcuateDistance(p, q):
     sum = 0
     for i in range(len(p)):
@@ -24,9 +24,50 @@ def calcuateDistance(p, q):
     return sum ** 0.5
 
 distanceMatrix = [[0 for x in range(rows)] for y in range(rows)]
+
 for i in range(rows):
     for j in range(rows):
         distanceMatrix[i][j] = calcuateDistance(matrix[i], matrix[j])
 distanceMatrix = np.asarray(distanceMatrix)
-print(distanceMatrix)
+
+clusterMatrix = np.array(distanceMatrix)
 #Calculated distance between two points
+
+clusters = []
+for i in range(rows):
+    clusters.append([i])
+
+#This method merges lists containing the following numbers
+def merge(p1, p2):
+    newCluster = []
+    newClusters = []
+    for cluster in clusters:
+        if p1 in cluster and p2 in cluster:
+            break
+        elif p1 in cluster:
+            newCluster += cluster
+        elif p2 in cluster:
+            newCluster += cluster
+        else:
+            newClusters.append(cluster)
+    newClusters.append(newCluster)
+    return newClusters
+
+
+def findMin(clusterMatrix):
+    min = sys.maxsize
+    p1 = 0
+    p2 = 0
+    for row in range(clusterMatrix.shape[0]):
+        for column in range(clusterMatrix.shape[1]):
+            if clusterMatrix[row][column] == 0.0:
+                continue
+            if clusterMatrix[row][column] < min:
+                min = clusterMatrix[row][column]
+                p1 = row
+                p2 = column
+
+    return min, p1, p2
+
+#min, p1, p2 = findMin(clusterMatrix)
+#print(min, ' ', p1, ' ', p2)
