40043190390541f1bec8909c14db256772c5623a
Ashwin Nikam <anikam@buffalo.edu>
2017-10-23 19:46:28 -0400

    Main commit

--- HierarchialClustering.py
+++ HierarchialClustering.py
@@ -2,6 +2,7 @@ import numpy as np
 import sys
 import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
+from sklearn.metrics.pairwise import euclidean_distances
 
 """
 Read the input file and get the number of rows and columns
@@ -25,38 +26,14 @@ true_values = np.array(matrix)[:, 0]
 matrix = np.array(matrix)
 matrix = np.delete(matrix, 0, 1)
 
-"""
-Create a ground truth matrix
-"""
-
-groundTruth = [[0 for x in range(rows)] for y in range(rows)]
-groundTruth = np.array(groundTruth)
-for row in range(groundTruth.shape[0]):
-    for column in range(groundTruth.shape[1]):
-        if true_values[row] == true_values[column]:
-            groundTruth[row][column] = 1
-        else:
-            groundTruth[row][column] = 0
-
-distanceMatrix = [[0 for x in range(rows)] for y in range(rows)]
+distanceMatrix = [[0.0 for x in range(rows)] for y in range(rows)]
 distanceMatrix = np.asarray(distanceMatrix)
 
+distanceMatrix = euclidean_distances(matrix, matrix)
 
-def calculateDistance(p, q):
-    sum = 0
-    for i in range(len(p)):
-        sum += (float(p[i]) - float(q[i])) ** 2
-    return sum ** 0.5
-
-
-for i in range(distanceMatrix.shape[0]):
-    for j in range(distanceMatrix.shape[1]):
-        distanceMatrix[i][j] = calculateDistance(matrix[i], matrix[j])
-
-clusterMatrix = np.array(distanceMatrix)
 clusters = []
 for i in range(rows):
-    clusters.append([i])
+    clusters.append([i+1])
 
 
 # This method merges lists containing the following numbers
@@ -80,30 +57,29 @@ def findMin():
     min = sys.maxsize
     p1 = 0
     p2 = 0
-    for row in range(clusterMatrix.shape[0]):
-        for column in range(clusterMatrix.shape[1]):
-            if clusterMatrix[row][column] == 0.0:
+    for row in range(distanceMatrix.shape[0]):
+        for column in range(distanceMatrix.shape[1]):
+            if distanceMatrix[row][column] == 0.0:
                 continue
-            if clusterMatrix[row][column] < min:
+            if distanceMatrix[row][column] < min:
                 flag = False
                 for cluster in clusters:  # Here we check if the two points aren't in the same cluster
-                    if row in cluster and column in cluster:
+                    if row+1 in cluster and column+1 in cluster:
                         flag = True
                 if flag == True:
                     continue
                 else:
-                    min = clusterMatrix[row][column]
-                    p1 = row
-                    p2 = column
-
+                    min = distanceMatrix[row][column]
+                    p1 = row+1
+                    p2 = column+1
     return min, p1, p2
 
+while(len(clusters) > 5):
+     min, p1, p2 = findMin()
+     clusters = merge(p1, p2)
+     print(len(clusters))
+print(clusters)
 
-# while(len(clusters) > 1):
-#     min, p1, p2 = findMin()
-#     clusters = merge(p1, p2)
-#     print(len(clusters))
-# print(clusters)
 
 def plotOriginalGraph(true_values):
     pca = PCA(n_components=2)
@@ -122,3 +98,49 @@ def plotOriginalGraph(true_values):
     plt.grid(True)
     plt.legend()
     plt.show()
+
+#plotOriginalGraph(true_values)
+
+
+groundTruth = [[0 for x in range(rows)] for y in range(rows)]
+groundTruth = np.array(groundTruth)
+for row in range(groundTruth.shape[0]):
+    for column in range(groundTruth.shape[1]):
+        if true_values[row] == true_values[column]:
+            groundTruth[row][column] = 1
+        else:
+            groundTruth[row][column] = 0
+
+clusterTruth = [[0 for x in range(rows)] for y in range(rows)]
+clusterTruth = np.array(clusterTruth)
+for row in range(rows):
+    for column in range(rows):
+        for cluster in clusters:
+            if row+1 in cluster and column+1 in cluster:
+                clusterTruth[row][column] = 1
+
+m11 = 0
+m00 = 0
+m01 = 0
+m10 = 0
+for row in range(groundTruth.shape[0]):
+    for column in range(groundTruth.shape[1]):
+        if groundTruth[row][column] == 1 and clusterTruth[row][column] == 1:
+            m11+=1
+        elif groundTruth[row][column] == 0 and clusterTruth[row][column] == 0:
+            m00+=1
+        elif groundTruth[row][column] == 0 and clusterTruth[row][column] == 1:
+            m01+=1
+        elif groundTruth[row][column] == 1 and clusterTruth[row][column] == 0:
+            m10+=1
+
+rIndex = (abs(m11) + abs(m00))/(abs(m00) + abs(m11) + abs(m01) + abs(m10))
+jCoefficient = abs(m11)/(abs(m11) + abs(m10) + abs(m01))
+print('Rand Index: ',rIndex)
+print('Jaccard Coefficient: ', jCoefficient)
+
+for i in range(len(clusters)):
+    for element in clusters[i]:
+        true_values[element-1] = i+1
+
+#plotOriginalGraph(true_values)
