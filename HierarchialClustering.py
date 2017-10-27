a24bf45514d6f29bc0d17bea17891c353589595a
Ashwin Nikam <anikam@buffalo.edu>
2017-10-27 16:50:25 -0400

    Tested with new data-set

--- HierarchialClustering.py
+++ HierarchialClustering.py
@@ -5,10 +5,15 @@ from sklearn.decomposition import PCA
 from sklearn.metrics.pairwise import euclidean_distances
 
 """
+Specify the final number of clusters you want
+"""
+k = 2
+
+"""
 Read the input file and get the number of rows and columns
 """
 
-file = open("../../Desktop/iyer.txt")
+file = open("../../Desktop/new_dataset_2.txt")
 lines = file.readlines()
 rows = len(lines)
 columns = len(lines[0].split("\t")) - 1
@@ -16,9 +21,8 @@ columns = len(lines[0].split("\t")) - 1
 """
 Create a data matrix consisting of all the gene attributes
 Create a separate true_values column containing all the ground truth
+Create a distance matrix storing euclidean distance between each point
 """
-
-k = 2
 
 matrix = [[0 for x in range(columns)] for y in range(rows)]
 for row in range(rows):
@@ -27,21 +31,31 @@ for row in range(rows):
 true_values = np.array(matrix)[:, 0]
 matrix = np.array(matrix)
 matrix = np.delete(matrix, 0, 1)
-
 distanceMatrix = [[0.0 for x in range(rows)] for y in range(rows)]
 distanceMatrix = np.asarray(distanceMatrix)
-
 distanceMatrix = euclidean_distances(matrix, matrix)
-
 clusters = []
 for i in range(rows):
     clusters.append([i])
 
 
-# This method merges lists containing the following numbers
+"""
+This method merges cluster containing point p1 with cluster 
+containing point p2.
+"""
+
+
 def merge(p1, p2):
     clusters[p1] = clusters[p1]+clusters[p2]
     clusters.pop(p2)
+
+
+"""
+p1 and p2 are points having lowest distance (min link)
+distanceMatrix is updated to merge rows and columns of
+p1 and p2 to generate new row and column which is p1 U p2
+"""
+
 
 def updateDistanceMatrix(p1, p2):
     row1 = distanceMatrix[p1]
@@ -55,6 +69,12 @@ def updateDistanceMatrix(p1, p2):
     return newMatrix
 
 
+"""
+findMin() finds the points in the distanceMatrix which are closest
+to each other and belong to different clusters.
+"""
+
+
 def findMin():
     min = sys.maxsize
     p1 = 0
@@ -65,16 +85,7 @@ def findMin():
                 min = distanceMatrix[row][column]
                 p1 = row
                 p2 = column
-    return min, p1, p2
-
-
-
-while(len(clusters) > k):
-     min, p1, p2 = findMin()
-     distanceMatrix = updateDistanceMatrix(p1, p2)
-     merge(p1, p2)
-     print(len(clusters))
-print(clusters)
+    return p1, p2
 
 
 def plotOriginalGraph(true_values):
@@ -95,7 +106,11 @@ def plotOriginalGraph(true_values):
     plt.legend()
     plt.show()
 
-plotOriginalGraph(true_values)
+
+while len(clusters) > k:
+     p1, p2 = findMin()
+     distanceMatrix = updateDistanceMatrix(p1, p2)
+     merge(p1, p2)
 
 
 groundTruth = [[0 for x in range(rows)] for y in range(rows)]
@@ -119,6 +134,7 @@ m11 = 0
 m00 = 0
 m01 = 0
 m10 = 0
+
 for row in range(groundTruth.shape[0]):
     for column in range(groundTruth.shape[1]):
         if groundTruth[row][column] == 1 and clusterTruth[row][column] == 1:
@@ -132,8 +148,9 @@ for row in range(groundTruth.shape[0]):
 
 rIndex = (abs(m11) + abs(m00))/(abs(m00) + abs(m11) + abs(m01) + abs(m10))
 jCoefficient = abs(m11)/(abs(m11) + abs(m10) + abs(m01))
-print('Rand Index: ',rIndex)
-print('Jaccard Coefficient: ', jCoefficient)
+print("For k = ", k);
+print('Rand Index: ', rIndex)
+print('Jaccard Coefficient: ', jCoefficient,'\n')
 
 for i in range(len(clusters)):
     for element in clusters[i]:
